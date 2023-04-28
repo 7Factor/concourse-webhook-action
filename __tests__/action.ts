4c01775c92288a889562f5d0148185710c1766db
Jeremy Wood <jeremy.wood@7factor.io>
2023-04-28 11:58:42 -0400

    Add a better base testing suite.

---
+++ __tests__/action.ts
@@ -0,0 +1,40 @@
+import * as os from 'os'
+import process from 'process'
+import path from 'path'
+import cp from 'child_process'
+
+export const runAction = (inputs: { [key: string]: string } | null = null): string => {
+  if (inputs != null) {
+    setActionInputs(inputs)
+  }
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env,
+  }
+
+  return cp.execFileSync(np, [ip], options).toString()
+}
+
+export const setActionInputs = (inputs: { [key: string]: string | null } = {}): void => {
+  for (const key in inputs) {
+    const value = inputs[key]
+    if (value != null) {
+      process.env[key] = value
+    } else {
+      delete process.env[key]
+    }
+  }
+}
+
+export const getActionOutput = (consoleOutput: string | Buffer, outputName: string): string | null => {
+  const output = consoleOutput.toString()
+  const regex = new RegExp(`::set-output name=${outputName}::(.*)${os.EOL}`)
+  const match = output.match(regex)
+  if (match) {
+    return match[1]
+  } else {
+    return null
+  }
+}
--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,28 @@
-import { wait } from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import { expect, test } from '@jest/globals'
+import { describe, expect, it, beforeAll } from '@jest/globals'
+import { getActionOutput, runAction, setActionInputs } from './action'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+describe('GitHub Action', () => {
+  describe('milliseconds input is provided', () => {
+    beforeAll(() => {
+      setActionInputs({ INPUT_MILLISECONDS: '500' })
+    })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  const delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+    it('outputs time', () => {
+      const output = runAction()
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env,
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+      const timeOutput = getActionOutput(output, 'time')
+      expect(timeOutput).not.toBeNull()
+      expect(timeOutput).toMatch(/\d{2}:\d{2}:\d{2}/)
+    })
+  })
+
+  describe('milliseconds input is NOT provided', () => {
+    beforeAll(() => {
+      setActionInputs({ INPUT_MILLISECONDS: null })
+    })
+
+    it('action fails', () => {
+      expect(runAction).toThrowError('Command failed')
+    })
+  })
 })
